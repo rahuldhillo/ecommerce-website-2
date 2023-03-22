c87bc5ef42f43906bfbf7960843947098e0ed25a
rahuldhillo <anemailformac@gmail.com>
2023-03-22 07:12:05 +0530

    used ngAfterViewInit for navbar toggle

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { faBagShopping } from '@fortawesome/free-solid-svg-icons';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { faBagShopping, faOutdent } from '@fortawesome/free-solid-svg-icons';
 import { HeaderService } from 'src/app/services/header.service';
 
 @Component({
@@ -8,14 +8,31 @@ import { HeaderService } from 'src/app/services/header.service';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  @ViewChild('navbarRef') navbarRef!: ElementRef;
+  @ViewChild('barRef') barRef!: ElementRef;
   faBagShopping = faBagShopping;
+  faOutdent = faOutdent;
   headerRouteTitles: any;
 
-  constructor(private headerService: HeaderService) {}
+  constructor(
+    private headerService: HeaderService,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit() {
     this.headerService.getJSON().subscribe((data) => {
       this.headerRouteTitles = data;
     });
+  }
+  ngAfterViewInit() {
+    const bar = this.barRef.nativeElement;
+    const navbar = this.navbarRef.nativeElement;
+
+    if (bar) {
+      console.log('bar click');
+      this.renderer.listen(bar, 'click', () => {
+        this.renderer.addClass(navbar, 'active');
+      });
+    }
   }
 }
